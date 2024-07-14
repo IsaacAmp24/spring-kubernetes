51dec1133d9a63c054e93cffb69dccaba1dac64a
IsaacAmp24 <ampudiaflores@gmail.com>
2024-07-14 03:31:29 -0500

    feat: class Main is modified.

--- msvc-users/src/main/java/org/amp/springcloud/msvc/users/MsvcUsersApplication.java
+++ msvc-users/src/main/java/org/amp/springcloud/msvc/users/MsvcUsersApplication.java
@@ -2,6 +2,7 @@ package org.amp.springcloud.msvc.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 public class MsvcUsersApplication {
