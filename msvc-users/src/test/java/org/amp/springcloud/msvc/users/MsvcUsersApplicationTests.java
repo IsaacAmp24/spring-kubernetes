f04da528105ee5ad18853d722e14a76899971040
IsaacAmp24 <ampudiaflores@gmailcom>
2024-07-13 19:23:39 -0500

    initial commit.

---
+++ msvc-users/src/test/java/org/amp/springcloud/msvc/users/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amp.springcloud.msvc.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
