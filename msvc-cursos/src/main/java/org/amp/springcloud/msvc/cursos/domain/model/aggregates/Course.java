bc22a5f8cbd99628fa0eb82c6cfe150f55c9e3f3
IsaacAmp24 <ampudiaflores@gmail.com>
2024-07-16 01:04:42 -0500

    feat(msvc-courses): class Course added.

---
+++ msvc-cursos/src/main/java/org/amp/springcloud/msvc/cursos/domain/model/aggregates/Course.java
@@ -0,0 +1,21 @@
+package org.amp.springcloud.msvc.cursos.domain.model.aggregates;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    //private String users;
+}
