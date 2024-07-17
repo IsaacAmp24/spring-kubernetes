0b47e523519a10c54c16f2b15e2f1f59d698011a
IsaacAmp24 <ampudiaflores@gmail.com>
2024-07-16 22:10:21 -0500

    refactor: rename package "msvc-cursos" -> "msvc-courses"

--- msvc-users/src/main/java/org/amp/springcloud/msvc/users/domain/model/aggregates/Users.java
+++ msvc-users/src/main/java/org/amp/springcloud/msvc/users/domain/model/aggregates/Users.java
@@ -1,6 +1,8 @@
 package org.amp.springcloud.msvc.users.domain.model.aggregates;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,10 +14,13 @@ public class Users {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotEmpty(message = "Name is required")
     private String name;
 
+    @Email(message = "Email is invalid")
     @Column(unique = true) // email is unique
     private String email;
 
+    @NotEmpty(message = "Password is required")
     private String password;
 }
