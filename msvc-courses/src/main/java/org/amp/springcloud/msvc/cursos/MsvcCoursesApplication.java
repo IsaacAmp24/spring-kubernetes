f88022998ca15a1653ddd166b1d35d91c57f06d1
IsaacAmp24 <ampudiaflores@gmail.com>
2024-07-17 23:17:44 -0500

    feat: add annotation for JPAAuditing

--- msvc-courses/src/main/java/org/amp/springcloud/msvc/cursos/MsvcCoursesApplication.java
+++ msvc-courses/src/main/java/org/amp/springcloud/msvc/cursos/MsvcCoursesApplication.java
@@ -2,7 +2,9 @@ package org.amp.springcloud.msvc.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MsvcCoursesApplication {
 
