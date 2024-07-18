ab9a97a527294db49faac4b2ca4fa03f5e8d0984
IsaacAmp24 <ampudiaflores@gmail.com>
2024-07-17 19:34:50 -0500

    feat: add auditing in msvcUsersApplication

--- msvc-users/src/main/java/org/amp/springcloud/msvc/users/MsvcUsersApplication.java
+++ msvc-users/src/main/java/org/amp/springcloud/msvc/users/MsvcUsersApplication.java
@@ -2,8 +2,10 @@ package org.amp.springcloud.msvc.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class MsvcUsersApplication {
 
 	public static void main(String[] args) {
