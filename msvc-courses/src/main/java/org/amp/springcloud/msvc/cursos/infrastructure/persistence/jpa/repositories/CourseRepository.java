24f013a02f7ec541c36f3b3c7e7b45f4312bc3a7
IsaacAmp24 <ampudiaflores@gmail.com>
2024-07-17 23:10:10 -0500

    refactor: add validation exist course by name.

--- msvc-courses/src/main/java/org/amp/springcloud/msvc/cursos/infrastructure/persistence/jpa/repositories/CourseRepository.java
+++ msvc-courses/src/main/java/org/amp/springcloud/msvc/cursos/infrastructure/persistence/jpa/repositories/CourseRepository.java
@@ -1,10 +1,12 @@
 package org.amp.springcloud.msvc.cursos.infrastructure.persistence.jpa.repositories;
 
 import org.amp.springcloud.msvc.cursos.domain.model.aggregates.Course;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends CrudRepository<Course, Long> {
+public interface CourseRepository extends JpaRepository<Course, Long> {
 
+    // no puede existir un curso con el mismo nombre
+    boolean existsByName(String name);
 }
