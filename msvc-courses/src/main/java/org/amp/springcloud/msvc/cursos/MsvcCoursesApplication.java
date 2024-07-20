e965bfd24759107cf22bab73e48b77eb7a2c22d3
IsaacAmp24 <ampudiaflores@gmail.com>
2024-07-20 18:22:32 -0500

    feat: add @FeignClients in class main of courses.

--- msvc-courses/src/main/java/org/amp/springcloud/msvc/cursos/MsvcCoursesApplication.java
+++ msvc-courses/src/main/java/org/amp/springcloud/msvc/cursos/MsvcCoursesApplication.java
@@ -2,10 +2,12 @@ package org.amp.springcloud.msvc.cursos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableFeignClients
 public class MsvcCoursesApplication {
 
 	public static void main(String[] args) {
